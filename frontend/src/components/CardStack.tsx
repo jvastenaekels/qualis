3394df8e20ace94b3496a21d6c88f49d7a63811c
Julien <julien.vastenaekels@gmail.com>
2026-01-19 23:31:17 +0100

    Aggressively increase rough sort card font size

--- frontend/src/components/CardStack.tsx
+++ frontend/src/components/CardStack.tsx
@@ -46,9 +46,9 @@ const CardStack: React.FC<CardStackProps & { ref?: React.Ref<CardStackHandle> }>
 
     // Dynamic Typography
     const textLength = statement.text.length;
-    let fontSizeClass = 'text-3xl @sm:text-4xl';
-    if (textLength > 150) fontSizeClass = 'text-lg @sm:text-xl';
-    else if (textLength > 80) fontSizeClass = 'text-2xl @sm:text-3xl';
+    let fontSizeClass = 'text-4xl @sm:text-5xl';
+    if (textLength > 150) fontSizeClass = 'text-xl @sm:text-2xl';
+    else if (textLength > 80) fontSizeClass = 'text-3xl @sm:text-4xl';
 
     // Overflow Detection
     useEffect(() => {
